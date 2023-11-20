29cbbeaa62b90a0864ea0a63610c4ae1152d5d55
mill-gai <pornratmill@gmail.com>
2023-11-20 16:59:23 -0500

    add column "file name"

--- newsapi.py
+++ newsapi.py
@@ -2,6 +2,8 @@ import requests
 import datetime
 import os
 import json
+import pandas as pd
+from functools import reduce
 
 from datetime import date
 
@@ -31,25 +33,67 @@ def fetch_latest_news(api_key, news_keywords, lookback=10):
 
     return data["articles"]
 
+
+def openFile(input_file):
+    # open json file and return as dict
+    with open(input_file) as json_file:
+        data = json.load(json_file)
+    return data
+
+def combineFileNameCol(series):
+    #return reduce(lambda x, y: [x] + [y] if type(x) is not list else x + [y], series)
+    return reduce(lambda x, y: x + y, series)
+
+def addFileNameCol(files):
+    # input is list of files
+    # convert each files into dataframe and add new column with the name of file 
+    all_data = []
+    for i in range(len(files)):
+        # get list of data of current file
+        data = openFile(files[i])
+        # for each data, add filename
+        for d in data:
+            d['filename'] = [files[i]]
+        # append to the main list
+        all_data = all_data + data
+
+    
+    # convert all_data to dataframe
+    df =  pd.DataFrame.from_dict(all_data)
+
+    col = ["title", "description", "url", "publishedAt", "filename"]
+    filter_col = ["title", "description", "url", "publishedAt"]
+    df = df[col]
+    new_df = df.groupby(filter_col).agg(combineFileNameCol)
+    return new_df
+    # new_df.to_csv('res2.tsv', sep="\t") 
+   
+
+
 if __name__ == '__main__':
     lookback = 30
     output = 'movies'
 
-    with open(os.path.join(output, "hunger_games"), 'w') as f:
-        json.dump(fetch_latest_news(API_KEY, ["The Ballad of Songbirds and Snakes"], lookback), f, indent=4)
-    with open(os.path.join(output, "the_marvels"), 'w') as f:
-        json.dump(fetch_latest_news(API_KEY, ["The Marvels"], lookback), f, indent=4)
-    with open(os.path.join(output, "anatomy_of_a_fall"), 'w') as f:
-        json.dump(fetch_latest_news(API_KEY, ["Anatomy of a Fall"], lookback), f, indent=4)    
-    with open(os.path.join(output, "barbie_movie"), 'w') as f:
-        json.dump(fetch_latest_news(API_KEY, ["Barbie"], lookback), f, indent=4)
-    with open(os.path.join(output, "oppenheimer"), 'w') as f:
-        json.dump(fetch_latest_news(API_KEY, ["Oppenheimer"], lookback), f, indent=4)
-    with open(os.path.join(output, "astroid_city"), 'w') as f:
-        json.dump(fetch_latest_news(API_KEY, ["Astroid City"], lookback), f, indent=4)
-    with open(os.path.join(output, "transformers"), 'w') as f:
-        json.dump(fetch_latest_news(API_KEY, ["Transformers Rise of the Beasts"], lookback), f, indent=4)
-    with open(os.path.join(output, "saltburn"), 'w') as f:
-        json.dump(fetch_latest_news(API_KEY, ["Saltburn"], lookback), f, indent=4)
-    with open(os.path.join(output, "trolls"), 'w') as f:
-        json.dump(fetch_latest_news(API_KEY, ["Trolls Band Together"], lookback), f, indent=4)
+
+
+
+
+    # with open(os.path.join(output, "hunger_games"), 'w') as f:
+    #     json.dump(fetch_latest_news(API_KEY, ["The Ballad of Songbirds and Snakes"], lookback), f, indent=4)
+    # with open(os.path.join(output, "the_marvels"), 'w') as f:
+    #     json.dump(fetch_latest_news(API_KEY, ["The Marvels"], lookback), f, indent=4)
+    # with open(os.path.join(output, "anatomy_of_a_fall"), 'w') as f:
+    #     json.dump(fetch_latest_news(API_KEY, ["Anatomy of a Fall"], lookback), f, indent=4)    
+    # with open(os.path.join(output, "barbie_movie"), 'w') as f:
+    #     json.dump(fetch_latest_news(API_KEY, ["Barbie"], lookback), f, indent=4)
+    # with open(os.path.join(output, "oppenheimer"), 'w') as f:
+    #     json.dump(fetch_latest_news(API_KEY, ["Oppenheimer"], lookback), f, indent=4)
+    # with open(os.path.join(output, "astroid_city"), 'w') as f:
+    #     json.dump(fetch_latest_news(API_KEY, ["Astroid City"], lookback), f, indent=4)
+    # with open(os.path.join(output, "transformers"), 'w') as f:
+    #     json.dump(fetch_latest_news(API_KEY, ["Transformers Rise of the Beasts"], lookback), f, indent=4)
+    # with open(os.path.join(output, "saltburn"), 'w') as f:
+    #     json.dump(fetch_latest_news(API_KEY, ["Saltburn"], lookback), f, indent=4)
+    # with open(os.path.join(output, "trolls"), 'w') as f:
+    #     json.dump(fetch_latest_news(API_KEY, ["Trolls Band Together"], lookback), f, indent=4)
+
