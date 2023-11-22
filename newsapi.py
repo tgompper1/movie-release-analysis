59d953ac2e66548d63a5b129184743a70fde15be
mill-gai <pornratmill@gmail.com>
2023-11-22 01:26:54 -0500

    rename to keyword, fix sort logic

--- newsapi.py
+++ newsapi.py
@@ -40,11 +40,14 @@ def openFile(input_file):
         data = json.load(json_file)
     return data
 
-def combineFileNameCol(series):
+def combineKeywordCol(series):
     #return reduce(lambda x, y: [x] + [y] if type(x) is not list else x + [y], series)
     return reduce(lambda x, y: x + y, series)
 
-def addFileNameCol(files):
+def keywordColSort(arr):
+    return (len(arr), arr)
+
+def addKeywordCol(files):
     # input is list of files
     # convert each files into dataframe and add new column with the name of file 
     all_data = []
@@ -53,22 +56,22 @@ def addFileNameCol(files):
         data = openFile(files[i])
         # for each data, add filename
         for d in data:
-            d['filename'] = [files[i][7:]] # [7:] -> remove the 'movies/' part
-            d['occurence'] = 1
+            d['keyword'] = [files[i][7:]] # [7:] -> remove the 'movies/' part
         # append to the main list
         all_data = all_data + data
 
-    
     # convert all_data to dataframe
     df =  pd.DataFrame.from_dict(all_data)
 
-    col = ["title", "description", "url", "publishedAt", "filename", "occurence"]
+    col = ["title", "description", "url", "publishedAt", "keyword"]
     filter_col = ["title", "description", "url", "publishedAt"]
     df = df[col]
-    new_df = df.groupby(filter_col).agg(combineFileNameCol).sort_values(by="occurence", ascending=False)
-    return new_df
+    new_df = df.groupby(filter_col).agg(combineKeywordCol)#.sort_values(by=["keyword", "occurence"], ascending=False)
+    new_df["sort_by_keyword"] = new_df["keyword"].apply(keywordColSort)
+    new_df = new_df.sort_values(by="sort_by_keyword", ascending=False)
+    return new_df.drop(columns=["sort_by_keyword"])
 
-def combineMovieData():
+def combineMovieData(output):
     # combine files under movies folder into one
     # get all files under movies(output) folder
     files_arr = [f for f in os.listdir(output) if os.path.isfile(os.path.join(output, f))]
@@ -76,16 +79,21 @@ def combineMovieData():
     for i in range(len(files_arr)):
         files_arr[i] = output + '/' + files_arr[i]
     
-    df = addFileNameCol(files_arr)
+    df = addKeywordCol(files_arr)
     #df.to_csv('res.tsv', sep="\t")
+
+# def selectData(n, df):
+#     # randomly select data from each keyword in a proportional way
+#     # get all distinct keywords and its count
+
+#     df_keyword = df["keyword"].value_counts()
 
 
 if __name__ == '__main__':
     lookback = 30
     output = 'movies'
 
-
-
+    combineMovieData(output)
 
 
     # with open(os.path.join(output, "hunger_games"), 'w') as f:
